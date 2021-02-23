ff48dc280fd204c7800c18ffc09bd05e04e0d248
ni9o <n13k@outlook.com>
2021-02-23 07:29:53 +0300

    Complex class

--- learnclass.py
+++ learnclass.py
@@ -1,9 +1,7 @@
-class MyClass:
-    i = 12345
+class Complex:
+    def __init__(self, realpart, imagpart):
+        self.r = realpart
+        self.i = imagpart
 
-    def f(self):
-        return 'hello world'
-
-x = MyClass()
-print(x.i)
-print(x.f())
+x = Complex(3.0, -4.5)
+print(x.r, x.i)
