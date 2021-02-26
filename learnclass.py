a8d1fabf2042296a2020704bd5cdb016df989010
ni9o <n13k@outlook.com>
2021-02-26 19:05:12 +0300

    Inheritance not done right

--- learnclass.py
+++ learnclass.py
@@ -1,7 +1,24 @@
-class Complex:
-    def __init__(self, realpart, imagpart):
-        self.r = realpart
-        self.i = imagpart
+class LivingBeing:
+    bookOfTheLiving = []
+    bookOfTheDead = []
+    lbID = None
 
-x = Complex(3.0, -4.5)
-print(x.r, x.i)
+    def __init__(self, lbID):
+        if lbID is None:
+            lbID = 0
+        else:
+            lbID += 1
+        self.bookOfTheLiving.append(lbID)
+        print('Born ', lbID)
+
+    def __del__(self, lbID):
+        self.bookOfTheDead.append(lbID)
+
+class Human(LivingBeing):
+    def __init__(self, mname):
+        self.name = mname
+
+for i in range(10):
+    x = Human(i)
+
+print(LivingBeing.bookOfTheLiving)
